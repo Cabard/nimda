c41122eaf9afff6e96c09fdb41101d8e929d6c35
Cabard <kadickov.andrei@yandex.ru>
2024-02-06 10:18:14 +0300

    first

---
+++ src/NimdaServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Cabard\Netbot;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class NimdaServiceProvider. Установщик пакета для Laravel
+ * @package Cabard\Netbot
+ */
+class NimdaServiceProvider extends ServiceProvider
+{
+    /**
+     * Загрузка любых служб пакета.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        dd(1);
+        config([
+            'auth.guards.nimda' => array_merge([
+                'driver' => 'nimda',
+                'provider' => null,
+            ], config('auth.guards.nimda', [])),
+        ]);
+
+        if (! app()->configurationIsCached()) {
+            $this->mergeConfigFrom(__DIR__.'/../config/nimda.php', 'nimda');
+        }
+    }
+
+    public function boot()
+    {
+        dd(2);
+//        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+//        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+        if (app()->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/nimda.php' => config_path('nimda.php'),
+            ], 'config');
+        }
+    }
+}
